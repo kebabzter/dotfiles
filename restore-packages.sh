99e39894d046b2e149d2c0596047e76c658f58f6
kebabzter <gabzterthesecond@gmail.com>
2025-11-26 13:56:10 +0100

    Added installation of yay to the restore script

--- restore-packages.sh
+++ restore-packages.sh
@@ -2,6 +2,20 @@
 
 backup_dir="$HOME/dotfiles"
 
+echo "Installing required tools..."
+sudo pacman -S --needed git base-devel
+
+echo "Installing yay..."
+if ! command -v yay >/dev/null 2>&1; then
+  git clone https://aur.archlinux.org/yay-bin.git
+  cd yay-bin
+  makepkg -si --noconfirm
+  cd ..
+  rm -rf yay-bin
+else
+  echo "yay is already installed."
+fi
+
 echo "Installing pacman packages..."
 sudo pacman -S --needed - < "$backup_dir/pkglist-pacman.txt"
 
