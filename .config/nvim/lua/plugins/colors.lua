1d5eda711cba0f67119dccf61a4e6481f6937759
kebabzter <gabzterthesecond@gmail.com>
2026-09-05 17:05:51 +0200

    changed color theme of nvim to some oasis thing

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -1,17 +1,34 @@
 return {
-    {
-       'rose-pine/nvim',
-       config = function()
-           vim.cmd.colorscheme "rose-pine"
-       end
+   -- {
+   --     'rose-pine/nvim',
+   --     config = function()
+   --         vim.cmd.colorscheme "rose-pine"
+   --     end
+   -- },
+   {
+      "uhs-robert/oasis.nvim",
+      lazy = false,
+      priority = 1000,
+      config = function()
+        require("oasis").setup({
+      style = "mirage",})      -- (see Configuration below for all customization options)
+        vim.cmd.colorscheme("oasis")  -- After setup, apply theme (or any style like "oasis-night")
+      end
    },
+   -- {
+   --    "ellisonleao/gruvbox.nvim",
+   --    priority = 1000 ,
+   --     config = function()
+   --         vim.cmd.colorscheme "gruvbox"
+   --     end
+   -- },
    {
        'nvim-lualine/lualine.nvim',
        dependencies = {
            "nvim-tree/nvim-web-devicons",
        },
        opts = {
-           theme = 'rose-pine',
+           theme = 'oasis',
        }
    }
 }
