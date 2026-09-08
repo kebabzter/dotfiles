bfa43979a6db39572c30f7e02f3b45bb5b70fae4
kebabzter <gabzterthesecond@gmail.com>
2026-09-08 17:41:55 +0200

    removed commented out code

--- .local/bin/scripts/open-pdf.sh
+++ .local/bin/scripts/open-pdf.sh
@@ -1,21 +1,3 @@
-#!/usr/bin/env bash
-#
-# # If an argument is provided, use it; otherwise search for PDFs using fzf
-# if [[ $# -eq 1 ]]; then
-#     selected=$1
-# else
-#     selected=$(find ~/Documents ~/books -type f -name "*.pdf" 2>/dev/null | fzf)
-# fi
-#
-# # Exit cleanly if no file was selected or found
-# if [[ -z $selected ]]; then
-#     exit 0
-# fi
-#
-# # Open the selected PDF in Zathura in the background, redirecting output
-# zathura "$selected" >/dev/null 2>&1 &
-#!/usr/bin/env bash
-
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
